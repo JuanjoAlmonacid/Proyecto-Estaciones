5e9414277c8773c656b9b95610b7709a8899d582
Juanjose Almonacid <almonacid305rgmail.com>
2021-07-31 22:20:59 -0400

    subiendo el proyecto

---
+++ Estaciones/Controllers/EstacionController.cs
@@ -0,0 +1,57 @@
+﻿using Estaciones.Dato;
+using Estaciones.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Estaciones.Controllers
+{
+    public class EstacionController : Controller
+    {
+        EstacionAdmin admin = new EstacionAdmin();
+        // GET: Estacion
+        public ActionResult Index()
+        {
+            IEnumerable<estacion> lista = admin.Consultar();
+            ViewBag.mensaje = "";
+            return View(lista);
+        }
+        public ActionResult Guardar() {
+            ViewBag.mensaje = "";
+            return View();
+        }
+
+        public ActionResult Nuevo(estacion modelo) {
+            admin.Guardar(modelo);
+            ViewBag.mensaje = "Estacion Guardada";
+            return View("Guardar",modelo);
+        }
+
+        public ActionResult Detalle(int id) {
+            estacion modelo = admin.Consultar(id);
+            ViewBag.mensaje = "";
+            return View(modelo);
+        }
+        public ActionResult Modificar(int id = 0) {
+            estacion modelo = admin.Consultar(id);
+            return View(modelo);
+        }
+        public ActionResult Actualizar(estacion modelo) {
+            admin.Modificar(modelo);
+            ViewBag.mensaje = "Estacion Modificado";
+            return View("Modificar", modelo);
+        }
+        public ActionResult Eliminar(int id = 0) {
+            estacion modelo = new estacion()
+            {
+                Id = id
+            };
+            admin.Eliminar(modelo);
+            IEnumerable<estacion> lista = admin.Consultar();
+            ViewBag.mensaje = "Estacion Eliminada";
+            return View("Index", lista);
+        }
+    }
+}
---
+++ Estaciones/Dato/EstacionAdmin.cs
@@ -0,0 +1,65 @@
+﻿using Estaciones.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Estaciones.Dato
+{
+    public class EstacionAdmin
+    {
+        public IEnumerable<estacion> Consultar()
+        {
+            ///<summary>
+            /// Consulta todos las estaciones
+            /// <returns>datos de los vehiculos</returns>
+
+            using (EstacionesdbEntities contexto=new EstacionesdbEntities()) {
+                return contexto.estacion.AsNoTracking().ToList();
+            
+            }
+        }
+
+        ///<summary>
+        ///Guardar estacion en la base de datos
+        /// </summary>
+        /// <param name="modelo"></param>
+        public void Guardar(estacion modelo) {
+            using (EstacionesdbEntities contexto = new EstacionesdbEntities()) {
+                contexto.estacion.Add(modelo);
+                contexto.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// Consulta
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public estacion Consultar(int id) {
+            using (EstacionesdbEntities contexto = new EstacionesdbEntities()) {
+                return contexto.estacion.FirstOrDefault(e => e.Id == id);
+            }
+        }
+        /// <summary>
+        /// Modifica los datos de la estacion
+        /// </summary>
+        /// <param name="modelo"></param>
+        public void Modificar(estacion modelo) {
+            using (EstacionesdbEntities contexto = new EstacionesdbEntities()) {
+                contexto.Entry(modelo).State = System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+            }
+        }
+        /// <summary>
+        /// Eliminar una estacion
+        /// </summary>
+        /// <param name="modelo"></param>
+        public void Eliminar(estacion modelo) {
+            using (EstacionesdbEntities contexto = new EstacionesdbEntities()) {
+                contexto.Entry(modelo).State = System.Data.Entity.EntityState.Deleted;
+                contexto.SaveChanges();
+            }
+        
+        }
+    }
+}
